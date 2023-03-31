fcec60eabb09278c6d8c68e5ae911d31defb5a0d
chetandabli <hrku002@gmail.com>
2023-04-01 01:06:10 +0530

    index page linked to other pages

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,8 +1,27 @@
 let params = new URL(document.location).searchParams;
 let token = params.get("token");
 let refresh_token = params.get("rtoken");
-localStorage.setItem("token", JSON.stringify(token));
-localStorage.setItem("refresh_token",refresh_token );
+const loginbtn = document.getElementById("my_ac_btn")
+document.getElementById("logo").onclick = ()=>{
+    location.assign("../index.html")
+}
+
+if(token){
+    localStorage.setItem("token", JSON.stringify(token));
+    localStorage.setItem("refresh_token",refresh_token );
+}
+
 const gettoken = JSON.parse(localStorage.getItem("token"))
-console.log(gettoken)
+
+if(gettoken){
+    loginbtn.innerText = "My Account"
+}
+
+loginbtn.onclick = ()=>{
+    if(loginbtn.innerText == "My Account"){
+        location.assign("../dashboard.html")
+    }else{
+        location.assign("../signup.html")
+    }
+}
 
