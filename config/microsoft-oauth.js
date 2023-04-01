a7b0f482a7ce89ab99b64dd685351ab5bf4540f8
ujjwal1309 <ujjwalsharma1309@gmail.com>
2023-04-01 10:03:45 +0530

    done

---
+++ config/microsoft-oauth.js
@@ -0,0 +1,32 @@
+const passport2 = require("passport");
+const AzureOAuth2Strategy = require("passport-azure-oauth").Strategy;
+const { Usermodel } = require("../models/user.model");
+
+const strategy = new AzureOAuth2Strategy(
+  {
+    clientID: "534626ad-2078-46b2-ad19-3fe5e7b409c2",
+    clientSecret: "74c9fb95-f6fd-4b53-ab66-f0b1fb647cff",
+    callbackURL: "http://localhost:3000/auth/microsoft/callback",
+    resource: "https://graph.microsoft.com/",
+  },
+  async (accessToken, refreshToken, profile, cb) => {
+    const { name, email, picture } = profile._json;
+    const userData = await Usermodel.find({ email });
+    console.log(userData);
+    if (userData.length) {
+      return cb(null, userData);
+    } else {
+      const newUser = new Usermodel({
+        name,
+        email,
+        picture,
+      });
+      await newUser.save();
+      return cb(null, newUser);
+    }
+  }
+);
+
+passport2.use(strategy);
+
+module.exports = { passport2 };
